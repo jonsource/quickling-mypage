4e0830cc03a3c174836a82cab62e25b01db384a1
Jan Šourek <jan.sourek@gmail.com>
2015-02-13 19:31:04 +0100

    local settings

---
+++ .env.local.php
@@ -0,0 +1,14 @@
+<?php
+
+/*----------------------------------------------------*/
+// Local environment vars
+/*----------------------------------------------------*/
+return array(
+
+    'DB_NAME'       => 'nepohrbivej',
+    'DB_USER'       => 'root',
+    'DB_PASSWORD'   => '',
+    'DB_HOST'       => 'localhost',
+    'WP_HOME'       => 'http://localhost/nepohrbivej/htdocs',
+    'WP_SITEURL'    => 'http://localhost/nepohrbivej/htdocs/cms',
+);
